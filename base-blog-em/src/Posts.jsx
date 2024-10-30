6fe965eca942976a2558293d9c6af027d1716dfc
CoryHurlbut <cory.hurlbut@wavelengthusaf.com>
2024-10-30 10:04:31 -0500

    Completed tutorial

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,40 +1,90 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { deletePost, fetchPosts, updatePost } from './api';
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+	const queryClient = useQueryClient();
 
-  return (
-    <>
-      <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
-            {post.title}
-          </li>
-        ))}
-      </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
-    </>
-  );
+	const deleteMutation = useMutation({
+		mutationFn: (postId) => deletePost(postId)
+	});
+
+	const updateMutation = useMutation({
+		mutationFn: (postId) => updatePost(postId)
+	});
+
+	useEffect(() => {
+		if (currentPage < maxPostPage) {
+			const nextPage = currentPage + 1;
+			queryClient.prefetchQuery({
+				queryKey: ['posts', nextPage],
+				queryFn: () => fetchPosts(nextPage)
+			});
+		}
+	}, [currentPage, queryClient]);
+
+	const { data, isError, error, isLoading } = useQuery({
+		queryKey: ['posts', currentPage],
+		queryFn: () => fetchPosts(currentPage),
+		staleTime: 2000 //2 seconds
+	});
+	if (isLoading) return <h3>Loading</h3>;
+	if (isError) {
+		return (
+			<>
+				<h3>Oops, something went wrong</h3>
+				<p>{error.toString()}</p>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<ul>
+				{data.map((post) => (
+					<li
+						key={post.id}
+						className='post-title'
+						onClick={() => {
+							deleteMutation.reset();
+							updateMutation.reset();
+							setSelectedPost(post);
+						}}>
+						{post.title}
+					</li>
+				))}
+			</ul>
+			<div className='pages'>
+				<button
+					disabled={currentPage <= 1}
+					onClick={() => {
+						setCurrentPage((previousValue) => previousValue - 1);
+					}}>
+					Previous page
+				</button>
+				<span>Page {currentPage}</span>
+				<button
+					disabled={currentPage >= maxPostPage}
+					onClick={() => {
+						setCurrentPage((previousValue) => previousValue + 1);
+					}}>
+					Next page
+				</button>
+			</div>
+			<hr />
+			{selectedPost && (
+				<PostDetail
+					post={selectedPost}
+					deleteMutation={deleteMutation}
+					updateMutation={updateMutation}
+				/>
+			)}
+		</>
+	);
 }
